471376c29e606cac76cb7490052b2f6161240158
Data-drone <bpl.law@gmail.com>
2020-12-12 22:20:15 +1100

    updated ray tune to use a scheduler

--- xgb_ray_tune.py
+++ xgb_ray_tune.py
@@ -1,7 +1,12 @@
 import sklearn.metrics
 from sklearn.model_selection import train_test_split
+
 import xgboost as xgb
+
 from ray import tune
+from ray.tune.integration.xgboost import TuneReportCheckpointCallback
+from ray.tune.schedulers import ASHAScheduler
+
 import pandas as pd
 
 def train_santander(config):
@@ -26,24 +31,35 @@ def train_santander(config):
         train_set,
         evals=[(test_set, "eval")],
         evals_result=results,
-        verbose_eval=False)
+        verbose_eval=False,
+        callbacks=[TuneReportCheckpointCallback(filename="model.xgb")])
     # Return prediction accuracy
-    accuracy = 1. - results["eval"]["error"][-1]
-    tune.report(mean_accuracy=accuracy, done=True)
+    #accuracy = 1. - results["eval"]["error"][-1]
+    #tune.report(mean_accuracy=accuracy, done=True)
 
 if __name__ == '__main__':
 
+# next step is to add a scheduler and gpu
+
     config = {
-         "objective": "binary:logistic",
-         "eval_metric": ["logloss", "error"],
-         "max_depth": tune.randint(1, 9),
-         "min_child_weight": tune.choice([1, 2, 3]),
-         "subsample": tune.uniform(0.5, 1.0),
-         "eta": tune.loguniform(1e-4, 1e-1)
+        "objective": "binary:logistic",
+        "eval_metric": ["auc", "error"],
+        "max_depth": tune.randint(1, 9),
+        "min_child_weight": tune.choice([1, 2, 3]),
+        "subsample": tune.uniform(0.5, 1.0),
+        "eta": tune.loguniform(1e-4, 1e-1)
     }
 
+    scheduler = ASHAScheduler(
+        max_t=10,  # 10 training iterations
+        grace_period=1,
+        reduction_factor=2)
+
     analysis = tune.run(
-         train_santander,
-         resources_per_trial={"cpu": 1},
-         config=config,
-         num_samples=10)
+        train_santander,
+        metric="eval-auc",
+        mode="max",
+        resources_per_trial={"cpu": 1, "gpu": 0.1},
+        config=config,
+        num_samples=10,
+        scheduler=scheduler)
